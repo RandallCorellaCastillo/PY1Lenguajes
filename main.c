b97263786d8db9f08ac3035546d99cb2faceb88a
Fredd Badilla Víquez <freddalessandro2876@gmail.com>
2023-08-20 16:50:58 -0600

    Menús

--- main.c
+++ main.c
@@ -2,3 +2,45 @@
 #include "cJSON.h"
 #include <stdlib.h>
 #include <string.h>
+
+//Declaracion de funciones
+void mainMenu();
+void operativeMenu();
+void generalMenu();
+
+
+//Inicio del programa
+int main() {
+    mainMenu();
+    return 0;
+};
+
+void mainMenu() {
+    int option;
+
+    printf("1. Menu operativo\n");
+    printf("2. Menu general\n");
+    printf("3. Salir\n");
+    printf("Ingrese su opcion: ");
+    scanf("%d", &option);
+    printf("\n");
+
+
+    if (option == 1) {
+        return operativeMenu();
+    } else if (option == 2) {
+        return generalMenu();
+    } else if (option == 3) {
+        return;
+    } else {
+        printf("Opcion incorrecta, intentalo de nuevo\n");
+    }
+};
+
+void operativeMenu() {
+    return;
+};
+
+void generalMenu() {
+    return;
+};
